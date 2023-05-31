7c85931e6a3d70dc0901a82e603bc959fc41b7e0
lazaro-pontes <laziruspoints@gmail.com>
2023-05-31 06:50:18 -0300

    cuidados com o tipo Number

---
+++ fundamentos/2-exercicios/NumberCuidados.js
@@ -0,0 +1,8 @@
+console.log(7 / 0) //não gera erro, mas sim o infinity
+console.log(`20` / 3) // funciona
+console.log(`show!` * 3) // gera erro, pois não é um number
+console.log(0.1 + 0.7) // gera uma imprecisão 
+
+//console.log(10.toString()) /* gera um erro*/
+
+console.log((10.345754).toFixed(2))
