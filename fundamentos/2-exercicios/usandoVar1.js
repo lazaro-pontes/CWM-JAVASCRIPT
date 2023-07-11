fd7669cc87ae55d541076e2b74f14c1aeb0e7c4d
lazaro-pontes <laziruspoints@gmail.com>
2023-07-11 03:43:28 -0300

    declaração de variaveis com Var

---
+++ fundamentos/2-exercicios/usandoVar1.js
@@ -0,0 +1,20 @@
+{
+    {
+        {
+            {
+                var sera = `sera que esta visivel?`
+                console.log(sera)
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+function teste() {
+    var local = 123
+    console.log(local)
+}
+
+teste()
+console.log(local) //erro
